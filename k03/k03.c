aa97d589e268ca3de78287af8b31bac3f525326e
kimucchi <shota0801.k@icloud.com>
2020-12-12 22:36:42 +0900

    コマンドの追加

--- k03/k03.c
+++ k03/k03.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define DEBUG
 
@@ -9,12 +10,74 @@ char StrKey[] = "wind";
 
 char* ForceSearch(char text[], char key[])
 {
+    int start,pos,text_len=strlen(text),key_len=strlen(key),correct;
+    for(start = 0; start <= (text_len - key_len); start++)
+    {
+        for(pos=0, correct = 0; pos <= key_len; pos++)
+        {
+             if(text[start + pos] == key[pos])
+             {
+                correct++;
+             }
+             if(correct == key_len)
+             {
+                 return text + start;
+             }
+        }
+    }
+    return NULL;
     //  ここを実装する
 
 }
 
 char* BMSearch(char text[], char key[])
 {
+   
+   char table[ALPHABET_LEN];
+   int n=0, index = strlen(key) - 1, key_len = strlen(key), pos = key_len - 1, index_before;
+
+   while(n <= ALPHABET_LEN)
+   {
+       table[n] = key_len;
+       n++;
+   }
+
+   int text_len = strlen(text);
+   n=0;
+
+   while(n < key_len)
+   {
+       table[(int)key[n]]  =key_len - 1 - n;
+       n++;
+   }
+
+   while(index <= text_len)
+   {
+       while(pos >= 0)
+       {
+           if(text[index] == key[pos])
+           {
+               if(pos == 0)
+               {
+                   return text + index;
+               }
+               index--;
+               pos--;
+           }
+           else
+           {
+               pos = key_len - 1;
+               break;
+           }
+       }
+       index_before = index;
+       index = index + table[(int)text[index]];
+       if(index_before > index)
+       {
+           index = index_before + 1;
+       }
+   }
+   return NULL;
     //  ここを実装する
 
 }
